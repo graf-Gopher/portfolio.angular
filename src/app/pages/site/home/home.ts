f7a0bdc7986a6b2eccc3118f57339e3985211c5d
graf-Gopher <sikora_kv@ukr.net>
2025-02-15 14:52:30 +0200

    dev_update_0.0.0

--- src/app/pages/site/home/home.ts
+++ src/app/pages/site/home/home.ts
@@ -1,11 +1,57 @@
 import { Component } from "@angular/core";
 import { SiteHeader } from "../../../shared/site/header/header";
+import { MatIconModule } from "@angular/material/icon";
+import { SiteFooter } from "@root/src/app/shared/site/footer/footer";
 
 @Component({
     selector: "app-home",
     standalone: true,
-    imports: [SiteHeader],
+    imports: [SiteHeader, SiteFooter, MatIconModule],
     templateUrl: "./home.html",
     styleUrl: "./home.scss",
 })
-export class HomePage {}
+export class HomePage {
+    public images: string[] = [
+        "https://www.relume.io/__assets/624380709031623bfe4aee60/6243807090316203124aee66_placeholder-image.svg",
+        "https://www.relume.io/__assets/624380709031623bfe4aee60/6243807090316203124aee66_placeholder-image.svg",
+        "https://www.relume.io/__assets/624380709031623bfe4aee60/6243807090316203124aee66_placeholder-image.svg",
+        "https://www.relume.io/__assets/624380709031623bfe4aee60/6243807090316203124aee66_placeholder-image.svg",
+    ];
+    public skills: { image: string; name: string }[] = [
+        { image: "https://www.relume.io/__assets/624380709031623bfe4aee60/6243807090316203124aee66_placeholder-image.svg", name: "Angular" },
+        { image: "https://www.relume.io/__assets/624380709031623bfe4aee60/6243807090316203124aee66_placeholder-image.svg", name: "Angular" },
+        { image: "https://www.relume.io/__assets/624380709031623bfe4aee60/6243807090316203124aee66_placeholder-image.svg", name: "Angular" },
+        { image: "https://www.relume.io/__assets/624380709031623bfe4aee60/6243807090316203124aee66_placeholder-image.svg", name: "Angular" },
+        { image: "https://www.relume.io/__assets/624380709031623bfe4aee60/6243807090316203124aee66_placeholder-image.svg", name: "Angular" },
+        { image: "https://www.relume.io/__assets/624380709031623bfe4aee60/6243807090316203124aee66_placeholder-image.svg", name: "Angular" },
+        { image: "https://www.relume.io/__assets/624380709031623bfe4aee60/6243807090316203124aee66_placeholder-image.svg", name: "Angular" },
+        { image: "https://www.relume.io/__assets/624380709031623bfe4aee60/6243807090316203124aee66_placeholder-image.svg", name: "Angular" },
+    ];
+    public projects: { image: string; name: string; description: string }[] = [
+        {
+            image: "https://www.relume.io/__assets/624380709031623bfe4aee60/6243807090316203124aee66_placeholder-image.svg",
+            name: "Date",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.",
+        },
+        {
+            image: "https://www.relume.io/__assets/624380709031623bfe4aee60/6243807090316203124aee66_placeholder-image.svg",
+            name: "Date",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.",
+        },
+        {
+            image: "https://www.relume.io/__assets/624380709031623bfe4aee60/6243807090316203124aee66_placeholder-image.svg",
+            name: "Date",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.",
+        },
+        {
+            image: "https://www.relume.io/__assets/624380709031623bfe4aee60/6243807090316203124aee66_placeholder-image.svg",
+            name: "Date",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.",
+        },
+        {
+            image: "https://www.relume.io/__assets/624380709031623bfe4aee60/6243807090316203124aee66_placeholder-image.svg",
+            name: "Date",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.",
+        },
+    ];
+}
--- src/app/shared/site/footer/footer.ts
+++ src/app/shared/site/footer/footer.ts
@@ -1,10 +1,13 @@
 import { Component } from "@angular/core";
+import { MatIconModule } from "@angular/material/icon";
+import { RouterModule } from "@angular/router";
+import { LangPipe } from "ngx-ute-core";
 
 @Component({
-    selector: "app-footer",
+    selector: "app-site-footer",
     templateUrl: "footer.html",
     styleUrls: ["footer.scss"],
     standalone: true,
-    imports: [],
+    imports: [LangPipe, MatIconModule, RouterModule],
 })
-export class Footer {}
+export class SiteFooter {}
